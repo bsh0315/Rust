20ad0047814aee7d8a74c6c1624c03bcdd1be9d8
bsh0315 <shbackrok@gmail.com>
2025-03-31 17:31:22 +0900

    copy, debug_format tests 추가

---
+++ basic/test1/Debug_Format.rs
@@ -0,0 +1,16 @@
+// Debug Format
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age : u32,
+}
+
+fn main(){
+    let mut person = Person{
+        name: String::from("엄준식"),
+        age : 30,
+    };
+    println!("Person : {:?}", person);
+
+
+}
